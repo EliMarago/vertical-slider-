ff4f6905a4cbf0010c8042eb6853c4c8e39e39fe
Elisabetta <156212818+EliMarago@users.noreply.github.com>
2025-04-28 18:07:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const sliderContainer = document.querySelector(".slider-container");
+const slideRight = document.querySelector(".right-slide");
+const slideLeft = document.querySelector(".left-slide");
+const btnUp = document.querySelector(".up-button");
+const btnDown = document.querySelector(".down-button");
+const slidesLength = slideRight.querySelectorAll("div").length;
+
+let activeSlide = 0;
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+btnUp.addEventListener("click", function () {
+  changeSlide("up");
+});
+btnDown.addEventListener("click", function () {
+  changeSlide("down");
+});
+function changeSlide(direction) {
+  const sliderHeight = sliderContainer.clientHeight;
+  if (direction === "up") {
+    activeSlide++;
+
+    if (activeSlide > slidesLength - 1) {
+      activeSlide = 0;
+    } else if (direction === "down") {
+      activeSlide--;
+    }
+    if (activeSlide < 0) {
+      activeSlide = slidesLength - 1;
+    }
+  }
+  slideRight.style.transform = `translateY(-${activeSlide * 100}vh)`;
+  slideLeft.style.transform = `translateY(${activeSlide * 100}vh)`;
+}
